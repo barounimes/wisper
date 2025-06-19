e265f828b84635f9c3017c902a150ee73f1f9e98
barounimes <ameurus392@gmail.com>
2025-06-19 15:30:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,5 @@ def keepalive():
     return f'Ping OK | Resultado: {result:.2f}', 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
